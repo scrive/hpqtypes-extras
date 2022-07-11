d3e8f9e195de4c84bfa93300496f19ef823fb93d
Jan Synáček <jan.synacek@scrive.com>
2022-07-11 09:54:56 +0200

    Revert "Set correct transaction settings"
    This reverts commit ab08f3708b6e4d970af192e90135330539eb6fa5.

--- src/Database/PostgreSQL/PQTypes/Checks.hs
+++ src/Database/PostgreSQL/PQTypes/Checks.hs
@@ -832,8 +832,6 @@ checkDBConsistency options domains tablesWithVersions migrations = do
 
         ModifyColumnMigration cursorSql updateSql batchSize -> do
           logMigration
-          ts <- getTransactionSettings
-          setTransactionSettings $ ts { tsIsolationLevel = Serializable }
           withCursorSQL "migration_cursor" NoScroll Hold cursorSql $ \cursor -> do
             fix $ \loop -> do
               cursorFetch_ cursor (CD_Forward batchSize)
@@ -842,7 +840,6 @@ checkDBConsistency options domains tablesWithVersions migrations = do
                 updateSql primaryKeys
                 commit
                 loop
-          setTransactionSettings ts
           updateTableVersion
 
       where
